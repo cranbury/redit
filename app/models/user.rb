93976c07c607fbeaf7e21ad61f35bd7c2370f409
Granger <GrangerAbuhoff@gmail.com>
2014-02-28 11:46:45 -0500

    pass user validation unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+
   has_secure_password
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe User do
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:username) }
-  it {should validate_presence_of(:password_digest) }
+
 end
 
