2b6c86385e080a66cc2156ce7c077298c20f0dd3
Granger <GrangerAbuhoff@gmail.com>
2014-02-28 12:03:23 -0500

    add root_path to sign in feature

--- spec/features/user_can_login_logout.rb
+++ spec/features/user_can_login_logout.rb
@@ -3,9 +3,11 @@ require 'spec_helper'
 describe "a user can login and logout" do
   let(:user) { FactoryGirl.create(:user)}
 
-  # it "creates a user" do
-  #   visit
-  # end
+  it "creates a user" do
+    visit root_path
+
+
+  end
 
 
 end
