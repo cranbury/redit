c332990fbc6ef9642920954d35b83659abd440b9
Jennifer <jennifergapay@gmail.com>
2014-02-28 11:33:26 -0500

    add user model

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    # user stuff
+    email { Faker::Internet.email }
+    username { Faker::Name.first_name }
+    password "password"
+    password_confirmation "password"
   end
 end
