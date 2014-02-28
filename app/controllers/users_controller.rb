bc51135a595fd3c647f8fb82042fd22465add4a0
Jennifer <jennifergapay@gmail.com>
2014-02-28 12:28:34 -0500

    add user's controller and views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render(:new)
+    end
+  end
+
+  def show
+  end
+  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation)
+  end
+
+
+end
